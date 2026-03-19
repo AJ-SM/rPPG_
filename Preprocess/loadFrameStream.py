b65cc65475776e12022718621f9ecd220842f96f
AJ-SM <anujsidam1108@gmail.com>
2026-03-19 19:36:37 +0530

    update the main thing

---
+++ Preprocess/loadFrameStream.py
@@ -0,0 +1,39 @@
+import os 
+import random
+# Dataset Path 
+
+PATH_Attack = r""
+PATH_Real = r""
+
+
+
+# Load Videos 
+attack = os.listdir(PATH_Attack)
+real = os.listdir(PATH_Real)
+
+attack_video = []
+real_video = []
+
+for file in attack:
+    f = os.path.join(attack,file)
+    attack.append(f)
+
+for file in real:
+    r = os.path.join(real,file)
+    real_video.append(r)
+
+random.shuffle(attack_video)
+random.shuffle(real_video)
+
+def sendVideoTrain(av=attack_video,rv=real_video):
+    r_int = random.random()
+    if r_int > 0.5:
+        r1 = random.choice(real_video)
+
+        return  real,True
+    else: 
+        r2 = random.choice(attack_video)
+        return r2,False
+    
+
+
